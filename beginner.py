f3803f1c9f2d6e474b8894af3a0227b4a92e0ea3
ZBegzod <zohidovbegzod4557@gmail.com>
2023-07-11 10:31:58 +0300

    draw triangles

--- beginner.py
+++ beginner.py
@@ -5,3 +5,44 @@ s.bgcolor("#FFCE30")
 s.setup(500, 500)
 t = turtle.Turtle()
 
+"""zed letter"""
+# t.forward(200)
+# t.left(40)
+
+# t.right(40)
+# t.forward(200)
+
+"""equilateral triangle"""
+# t.forward(100)
+# t.lt(120)
+# t.forward(100)
+# t.lt(120)
+# t.forward(100)
+
+
+"""right angle triangle """
+t.rt(180)
+t.forward(150)
+t.rt(90)
+t.forward(200)
+t.rt(143)
+t.forward(250)
+
+"""an acute-angled triangle"""
+# t.lt(10)
+# t.forward(150)
+# t.lt(120)
+# t.forward(100)
+# t.lt(100)
+# t.forward(140)
+
+
+# t.forward(100)
+# t.lt(150)
+# t.forward(150)
+# t.lt(140)
+# t.forward(90)
+
+# t.undo()
+# turtle.done
+turtle.exitonclick()
